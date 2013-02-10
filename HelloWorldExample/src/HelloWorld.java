57024907c001fb9c58c3d8c943599130907d3057
Shaun Miller <sam394@psu.edu>
2013-02-10 14:55:14 -0500

    New2

--- HelloWorldExample/src/HelloWorld.java
+++ HelloWorldExample/src/HelloWorld.java
@@ -11,6 +11,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// This is my program area
 		System.out.println("MusicNow Rocks!");
+		System.out.println("MusicNow is the Best App!");
 		
 	}
 
