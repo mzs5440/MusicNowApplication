dbd710b3d46a34da221e0310edcb04b9ee32c0b0
Shaun Miller <sam394@psu.edu>
2013-02-10 13:58:48 -0500

    New

---
+++ HelloWorldExample/src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * Prints Hello World to Console
+ * @author Shaun A Miller
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// This is my program area
+		System.out.println("MusicNow Rocks!");
+		
+	}
+
+}
